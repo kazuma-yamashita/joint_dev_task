479f5c4343a1e4f77ee309a1029cc5ddf11f0f87
kazuma yamashita <wish.kzm@icloud.com>
2020-04-29 23:06:31 +0900

    ruby基礎Q8~11 回答

--- task.rb
+++ task.rb
@@ -64,8 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
-
+  programming_languages = programming_languages.map(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,21 +75,37 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  number = 1
+  names.each do |name|
+    puts "会員No.#{number} #{name}さん"
+    number += 1
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports = sports.flatten
+  puts "ユーザーの趣味一覧"
+  number = 1
+  sports.uniq.each do |name|
+    puts "No#{number} #{name}"
+    number += 1
+  end
 end
 
 def q12
