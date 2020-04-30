461b5823cfd36db34e563599ff14d23f1d1451a3
kazuma yamashita <wish.kzm@icloud.com>
2020-05-01 06:36:18 +0900

    ruby Q8~11 修正

--- task.rb
+++ task.rb
@@ -64,7 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -75,10 +75,8 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  number = 1
-  names.each do |name|
-    puts "会員No.#{number} #{name}さん"
-    number += 1
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
   end
 end
 
@@ -87,24 +85,17 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food.include?("うに")
-      puts "好物です"
-    else
-      puts "まぁまぁ好きです"
-    end
+    puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
   end
 end
-
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports = sports.flatten
+
   puts "ユーザーの趣味一覧"
-  number = 1
-  sports.uniq.each do |name|
-    puts "No#{number} #{name}"
-    number += 1
+  sports.flatten.uniq.each.with_index(1) do |name, i|
+    puts "No#{i} #{name}"
   end
 end
 
@@ -112,7 +103,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  p data
 end
 
 def q13
