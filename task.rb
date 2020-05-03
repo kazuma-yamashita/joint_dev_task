266930f99a4d54544b6570541b8c6875c6641ba7
kazuma yamashita <wish.kzm@icloud.com>
2020-05-03 13:32:39 +0900

    ruby基礎 Q12~16 回答

--- task.rb
+++ task.rb
@@ -103,7 +103,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-  p data
+  p data[:user][:name]
 end
 
 def q13
@@ -111,13 +111,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  p user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  key = data.keys
+  p key
 
 end
 
@@ -126,7 +129,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  p data1.key?(:age)? "OK" : "NG"
+  p data2.key?(:age)? "OK" : "NG"
 end
 
 def q16
@@ -138,7 +142,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
